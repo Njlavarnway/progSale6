0558677605e98d06b7bd30079349bd9b6d493e72
Njlavarnway <64217657+Njlavarnway@users.noreply.github.com>
2023-12-06 11:42:22 -0500

    Update program6.java

--- program6.java
+++ program6.java
@@ -1,5 +1,86 @@
 package salerecordpack;
 
+import java.util.*;
+
 public class program6 {
 
+	/**
+	 * Reads Sale Records in TreeMap.
+	 * Uses a Map of Car Make with values as TreeMaps.
+	 * Handles duplicates in the inner TreeMap.
+	 *
+	 * @param filePath Path to the CSV file.
+	 * @return A Map of Car Make with a TreeMap of sales dates and lists of sales records.
+	 */
+	public static Map<String, TreeMap<String, List<SaleRecord>>> readSaleRecords(String filePath) {
+		// Implementation goes here
+		// ...
+
+		return null; // Placeholder
+	}
+
+	/**
+	 * Provides a menu for user input.
+	 * Enables the user to enter a date and a Car Make.
+	 * Calculates the number of cars sold on and after that date.
+	 *
+	 * @param saleRecordsMap Map of Car Make with TreeMap of sales records.
+	 */
+	public static void userMenu(Map<String, TreeMap<String, List<SaleRecord>>> saleRecordsMap) {
+		// Implementation goes here
+		// ...
+
+	}
+
+	/**
+	 * Generates diagrams for time taken for insertion and search per number of items.
+	 * X-axis: Number of items (1 to 2.5M).
+	 * Y-axis: Time needed for insertion/search.
+	 * Provides data for both TreeMap and Binary Search Tree.
+	 */
+	public static void generateDiagrams() {
+		// Implementation goes here
+		// ...
+
+	}
+
+	/**
+	 * Discusses and analyzes the diagrams.
+	 * References the diagrams in the discussion.
+	 */
+	public static void discussDiagrams() {
+		// Implementation goes here
+		// ...
+
+	}
+
+	/**
+	 * Main method that takes the CSV file path as an input argument.
+	 * Prints runtime for each sub-task.
+	 *
+	 * @param args Command-line arguments.
+	 */
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: java Program6 <CSVFilePath>");
+			System.exit(1);
+		}
+
+		String filePath = args[0];
+
+		long startTime = System.currentTimeMillis();
+
+		Map<String, TreeMap<String, List<SaleRecord>>> saleRecordsMap = readSaleRecords(filePath);
+		userMenu(saleRecordsMap);
+		generateDiagrams();
+		discussDiagrams();
+
+		long endTime = System.currentTimeMillis();
+		System.out.println((endTime - startTime) + " milliseconds to execute the program.");
+	}
+
+	// SaleRecord class represents a single sale record
+	static class SaleRecord {
+		// Fields and methods for SaleRecord
+	}
 }
